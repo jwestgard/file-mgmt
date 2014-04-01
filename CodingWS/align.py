45411d05d81799e650a089fe3fdcf3c81191cdeb
Joshua Westgard <jwestgard@mac.com>
2014-04-01 16:18:42 -0400

    new data, add coding workshop folder

---
+++ CodingWS/align.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+    
+def alignlists(list1, list2):
+    result = []
+    col1 = iter(sorted(list1))
+    col2 = iter(sorted(list2))
+    x = next(col1)
+    y = next(col2)
+    while col1 and col2:
+        if x == None and y == None:
+            break
+        if x == None:
+            while y:
+                result.append("\t" + str(y))
+                y = next(col2, None)
+        elif y == None:
+            while x:
+                result.append(str(x) + "\t")
+                x = next(col1, None)
+        elif x == y:
+            result.append(str(x) + "\t" + str(y))
+            x = next(col1, None)
+            y = next(col2, None)
+        elif x > y:
+            result.append("\t" + str(y))
+            y = next(col2, None)
+        elif x < y:
+            result.append(str(x) + "\t")
+            x = next(col1, None)
+    return result
+
+local_set = set()
+fedora_set = set()
+
+for line in open('prangeasset.txt'):
+    local_set.add(line.rstrip("\n").split("\t")[0])
+
+for line in open('pcbinfedora.txt'):
+    fedora_set.add(line.rstrip("\n"))
+                 
+result = alignlists(local_set, fedora_set)
+
+for i in result:
+	print(i)
---
+++ listfiles.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+
+def listfiles(dir):
+    for path, dnames, fnames in os.walk(dir):
+    	for f in fnames:
+    		print("{0}\t{1}\t{2}".format(f, os.path.getsize(os.path.join(path, f)), path))
+
+def main():
+    listfiles(sys.argv[1])
+    
+if __name__ == "__main__":
+	main()
